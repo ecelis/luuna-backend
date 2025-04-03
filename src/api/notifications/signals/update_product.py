f6c35dec8665fb729bb91780802e26d360bcd136
Ernesto Celis <ernesto@celisdelafuente.net>
2025-04-03 10:20:37 -0600

    fix(notifications): remove hardcoded strings

--- src/api/notifications/signals/update_product.py
+++ src/api/notifications/signals/update_product.py
@@ -1,5 +1,6 @@
 import os
 
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from products.models.product import Product
@@ -11,7 +12,10 @@ from ..email.email_service import EmailService
 def save_product(sender, instance, **kwargs):
     if sender is Product:
         content = f"{instance.id} - {instance.name} - {instance.price} was updated at {instance.updated_at}"
-        payload = {"user_email": "empleado2@gosoftlive.com"}
+        staff_emails = User.objects.filter(is_staff=True).values_list(
+            "email", flat=True
+        )
+        payload = {"user_email": ",".join(staff_emails)}
         email_service = EmailService(payload, channel=os.getenv("EMAIL_CHANNEL", None))
         mailer = email_service.get_mailer()
-        mailer.send_email(subject="Prueba", content=content)
+        mailer.send_email(subject="Product updated", content=content)
